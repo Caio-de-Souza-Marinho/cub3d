6ea5aaf801c1e1d81311b77ade76cc06993d71ac
Marcus Vinicius Dos Santos Gomes <marcudos@c1r9p4.42sp.org.br>
2025-07-09 17:55:42 -0300

    start parsing

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 19:37:37 by caide-so          #+#    #+#             */
-/*   Updated: 2025/07/08 19:45:33 by caide-so         ###   ########.fr       */
+/*   Updated: 2025/07/09 17:38:38 by marcudos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,21 @@
 
 # include <stdio.h>
 
+// Stucts
+typedef struct s_color
+{
+	int	r;
+	int	g;
+	int	b;
+}	t_color;
+
+typedef struct s_config
+{
+	t_color	floor;
+	t_color	ceiling;
+}	t_config;
+
+
+t_config	*init_cub();
+int		parse_cub(char *filename, t_config *cfg);
 #endif
--- src/main.c
+++ src/main.c
@@ -6,16 +6,25 @@
 /*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 19:21:33 by caide-so          #+#    #+#             */
-/*   Updated: 2025/07/08 19:21:54 by caide-so         ###   ########.fr       */
+/*   Updated: 2025/07/09 17:42:48 by marcudos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
+#include <stdlib.h>
 
 int	main(int argc, char **argv)
 {
+	t_config	*cfg;
+
 	(void)argc;
 	(void)argv;
 	printf("oi cub3d\n");
+	cfg = (t_config *) malloc(sizeof(t_config));
+	if (!parse_cub(argv[1], cfg))
+	{
+		printf("Invalid Map");
+		return (1);
+	}
 	return (0);
 }
