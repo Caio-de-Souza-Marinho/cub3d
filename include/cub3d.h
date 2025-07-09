fff344d5a5d311d53f8ac2a61aafad91511d57f8
Caio De Souza Marinho <caide-so@c1r10p4.42sp.org.br>
2025-07-09 20:36:31 -0300

    feature: map validation: validate multiple players start position and all possible characters

--- include/cub3d.h
+++ include/cub3d.h
@@ -13,6 +13,60 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "../lib/libft/include/libft.h"
+# include "../lib/minilibx-linux/mlx.h"
+
+# include <math.h>
+
+// printf, perror
 # include <stdio.h>
+
+// open
+# include <fcntl.h>
+
+// close, read, write
+# include <unistd.h>
+
+// strerror
+# include <string.h>
+
+// malloc, free, exit
+# include <stdlib.h>
+
+// gettimeofday
+# include <sys/time.h>
+
+typedef struct s_color {
+	int	r;
+	int	g;
+	int	b;
+}	t_color;
+
+typedef struct s_texture {
+	char	*no;
+	char	*so;
+	char	*we;
+	char	*ea;
+}	t_texture;
+
+typedef struct s_map {
+	char	**grid;
+	int		width;
+	int		height;
+}	t_map;
+
+typedef struct s_player {
+	int		x;
+	int		y;
+	char	dir;
+}	t_player;
+
+typedef struct s_config {
+	t_texture	texture;
+	t_color		floor;
+	t_color		ceiling;
+	t_map		map;
+	t_player	player;
+}	t_config;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -12,10 +12,98 @@
 
 #include "../include/cub3d.h"
 
-int	main(int argc, char **argv)
+char	**create_test_map(int *height);
+int	validate_map(char **grid);
+int	validate_chars(char c);
+void	free_map(t_config *config);
+int	in(const char *s, char c);
+
+int	main(void)
 {
-	(void)argc;
-	(void)argv;
-	printf("oi cub3d\n");
+	t_config	config;
+
+	config.map.grid = create_test_map(&config.map.height);
+	config.map.width = 6;
+	config.player.x = 4;
+	config.player.y = 2;
+	config.player.dir = 'N';
+	
+	if (validate_map(config.map.grid))
+		printf("Map is valid\n");
+	else
+		printf("Map is invalid\n");
+
+	free_map(&config);
+	return (0);
+}
+
+void	free_map(t_config *config)
+{
+	int	i;
+
+	i = 0;
+	while (i < config->map.height)
+	{
+		free(config->map.grid[i]);
+		i++;
+	}
+	free(config->map.grid);
+}
+
+char	**create_test_map(int *height)
+{
+	char	**grid;
+
+	grid = (char **)malloc(sizeof(char *) * 6);
+	if (!grid)
+		return (NULL);
+	grid[0] = ft_strdup("111111");
+	grid[1] = ft_strdup("100001");
+	grid[2] = ft_strdup("1000N1");
+	grid[3] = ft_strdup("100001");
+	grid[4] = ft_strdup("111111");
+	grid[5] = NULL;
+	*height = 5;
+	return (grid);
+}
+
+int	validate_map(char **grid)
+{
+	int	y;
+	int	x;
+	int	start_pos;
+
+	y = 0;
+	start_pos = 0;
+	while (grid[y])
+	{
+		x = 0;
+		while (grid[y][x])
+		{
+			if (!in("01NSEW", grid[y][x]))
+				return (0);
+			if (in("NSEW", grid[y][x]))
+				start_pos++;
+			if (start_pos > 1)
+				return (0);
+			x++;
+		}
+		y++;
+	}
+	return (1);
+}
+
+// Checks if a character exists in a string
+// 1. Iterates through s to find c
+int	in(const char *s, char c)
+{
+	if (s == NULL)
+		return (0);
+	while (*s)
+	{
+		if (*s == c)
+			return (1);
+		s++;
+	}
 	return (0);
 }
